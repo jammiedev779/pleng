181cbc0123efb1686b67d0b520b47fe43dff641a
Jammie779 <vinunsombath@gmail.com>
2024-07-04 22:03:41 +0700

    sthing for plorck leang

--- lib/group/library/library.dart
+++ lib/group/library/library.dart
@@ -1,22 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:pleng/group/headbar/head_bar.dart';
 
-class Library extends StatelessWidget {
-  // const HomeScreen({super.key});
+class Library extends StatefulWidget {
+  @override
+  _LibraryState createState() => _LibraryState();
+}
 
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+class _LibraryState extends State<Library> with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: _buildBody(),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: HeadBar(),
+        body: _selectedIndex == 1
+            ? SearchScreen()
+            : TabBarView(
+                controller: _tabController,
+                children: [
+                  MusicScreen(tabController: _tabController),
+                  Center(child: Text('Videos Screen')),
+                  Center(child: Text('My Device Screen')),
+                ],
+              ),
+      ),
     );
   }
+}
 
-  Widget _buildBody() {
-    return Container(
-      color: Colors.yellow,
-      alignment: Alignment.center,
+class MusicScreen extends StatelessWidget {
+  final TabController tabController;
+
+  MusicScreen({required this.tabController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TabBar(
+            controller: tabController,
+            tabs: [
+              Tab(text: 'All'),
+              Tab(text: 'Playlists'),
+              Tab(text: 'Albums'),
+              Tab(text: 'Podcasts'),
+            ],
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            indicator: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Column(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  label: Text('Create a playlist'),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Icon(Icons.music_note, size: 100, color: Colors.white),
+                SizedBox(height: 10),
+                Text(
+                  'Nothing has been Added',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Browse songs, playlists, podcasts and albums to add to your favourites',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Explore Now'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    textStyle: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Search Screen'),
     );
   }
 }
--- lib/group/search/search.dart
+++ lib/group/search/search.dart
@@ -11,7 +11,18 @@ class _SearchState extends State<Search> {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwme89cM8YZvHcybGrZl_Obd9U9p5QabozJQ&s",
     "https://play-lh.googleusercontent.com/jInS55DYPnTZq8GpylyLmK2L2cDmUoahVacfN_Js_TsOkBEoizKmAl5-p8iFeLiNjtE=w526-h296-rw",
     "https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg",
+    "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1516117172878-fd2c41f4a759?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1541698444083-023c97d3f4b6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDN8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1523240795612-9a054b0db644?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDd8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDl8fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1519985176271-adb1088fa94c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEwfHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDEyfHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDIzfHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
+    "https://images.unsplash.com/photo-1523240795612-9a054b0db644?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDI1fHBob3RvfGVufDB8fHx8MTY0MzM4OTUxOQ&ixlib=rb-1.2.1&q=80&w=400",
   ];
+
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -40,7 +51,7 @@ class _SearchState extends State<Search> {
                   decoration: InputDecoration(
                     hintText: 'Music, Podcast, Artist and more',
                     hintStyle: TextStyle(
-                        color: Colors.white), // Set hint text color to white
+                        color: Colors.white),
                     prefixIcon: Icon(Icons.search, color: Colors.white),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(30),
@@ -50,7 +61,7 @@ class _SearchState extends State<Search> {
                     fillColor: Colors.grey[800],
                   ),
                   style: TextStyle(
-                      color: Colors.white), // Set input text color to white
+                      color: Colors.white),
                 ),
               ),
               Padding(
@@ -73,7 +84,7 @@ class _SearchState extends State<Search> {
                   crossAxisSpacing: 8,
                   childAspectRatio: 1,
                 ),
-                itemCount: images.length, // Adjust based on the number of items
+                itemCount: images.length,
                 itemBuilder: (context, index) {
                   return _buildRecommendedItem(index);
                 },
